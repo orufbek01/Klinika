5266f5cf31d6472c8d88d3e9359fdd1650ab4744
orufbek01 <vohobovmansurbek0@gmial.com>
2024-02-17 20:57:37 +0300

    Orufbek

--- home/urls.py
+++ home/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('main.urls')),
+    path('main/', include('main.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('account/', include('account.urls'))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
