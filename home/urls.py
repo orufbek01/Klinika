e27d179fdfc9a0103b6accf473af2de1247414d1
orufbek01 <vohobovmansurbek0@gmial.com>
2024-03-02 16:38:13 +0300

    Orufbek

--- home/urls.py
+++ home/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
     path('dashboard/', include('dashboard.urls')),
-    path('account/', include('account.urls'))
+    path('account/', include('account.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
